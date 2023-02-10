cbc1f4d4f4e7d8d1c1f7f0427ebc1e4bf6ea8877
rudmil00 <milanrudanzr@gmail.com>
2023-02-10 01:58:48 +0100

    card css
    Signed-off-by: rudmil00 <milanrudanzr@gmail.com>

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,27 @@ import React, { useState } from 'react';
 const Card = ({ card }) => {
     const [flip, setflip] = useState(false); // okretanje kartice
     return (
-        <div onClick={() => setflip(!flip)} >
+
+        < div
+            className={`card ${flip ? 'flip' : ''}`}
+
+            onClick={() => setflip(!flip)} >
+            <div className='front'>
+                {card.question}
+                <div className='card-options'>
+                    {card.options.map(option => {
+                        return <div className='card-option'>{option} </div>;
+                    })}
+                </div>
+
+
+            </div>
+
+            <div className="back"> {card.answer}</div>
             {flip ? card.answer : card.question}
 
-        </div>
+        </div >
+
     );
 };
 
--- src/components/Cardlist.js
+++ src/components/Cardlist.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from './Card';
 const Cardlist = ({ cardlist }) => {
     return (
-        <div className='card-div'>
+        <div className='card-grid'>
             {cardlist.map(card => {
                 return <Card card={card} key={card.id} />; //key koristimo da ne  bi renderovao ono sto vec jeste
             })}
